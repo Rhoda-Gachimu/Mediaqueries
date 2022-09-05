5a7862c36990230304122516d33548b81b4df8fa
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-09-05 22:06:51 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+// SUBSCRIBE
+
+const subButton=document.getElementById("subscribe")
+const email=document.getElementById("email")
+
+subButton.addEventListener("click",function (){
+    email.value="";
+    const paragraph=document.getElementById("thank")
+    paragraph.textContent="Thank you for subscribing!"
+    })
+
+
+    // API
+
+const joke= document.getElementById("viewjokes")
+const jokeContent = document.getElementById("in")
+
+
+joke.addEventListener("click", function() {
+    callApi()
+})
+
+function callApi() {
+    fetch('https://api.chucknorris.io/jokes/random')
+      .then((response) => response.json())
+      .then((data) => {
+        jokeContent.textContent=data.value
+      })
+}
